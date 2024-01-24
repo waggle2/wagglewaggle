2ab42343030c22fbc2e460d8e57dce6624b5de05
hitzza <cslzh4433@gmail.com>
2024-01-24 14:32:39 +0900

    Rename : add common folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Footer from './_component/footer/page'
-import Header from './_component/header/page'
+import Footer from './_component/common/footer/page'
+import Header from './_component/common/header/page'
 import Image from '@/node_modules/next/image'
 import logo from '../public/logo.svg'
-import Search from './_component/header/_component/Search'
-import Bell from './_component/header/_component/Bell'
+import Search from './_component/common/header/_component/Search'
+import Bell from './_component/common/header/_component/Bell'
 import SwipeEvent from './_component/eventSlide/SwipeEvent'
 
 export default function Home() {
