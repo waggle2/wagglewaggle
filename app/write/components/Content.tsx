3c6b06eacf9620e05bc5abfa102d8fd05481fdca
BoyoungYun <index_1234@naver.com>
2024-01-25 00:02:57 +0900

    Design: Add ImageContainer and Resize textarea

--- app/write/components/Content.tsx
+++ app/write/components/Content.tsx
@@ -1,16 +1,24 @@
 'use client'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import styles from '../styles/content.module.scss'
 import NavBar from './NavBar'
 import Dropdown from './Dropdown'
 import Footer from './Footer'
 import VoteContainer from './VoteContainer'
+import ImageContainer from './ImageContainer'
 export default function Content() {
   const [isToggle, setIsToggle] = useState(false)
   const [isImage, setIsImage] = useState(false)
   const [isVote, setIsVote] = useState(false)
   const [content, setContent] = useState('')
   const tag = ['# 공감해줘', '# 조언해줘', '# 골라줘', '# 수다수다']
+  const textarea = useRef<HTMLTextAreaElement | null>(null)
+  const handleResizeHeight = () => {
+    if (textarea.current) {
+      textarea.current.style.height = 'auto'
+      textarea.current.style.height = textarea.current.scrollHeight + 'px'
+    }
+  }
   return (
     <div>
       <NavBar isToggle={isToggle} setIsToggle={setIsToggle} />
@@ -35,10 +43,16 @@ export default function Content() {
       </div>
       <div className={styles.contentBox}>
         {isVote && <VoteContainer setIsVote={setIsVote} />}
+        {isImage && <ImageContainer setIsImage={setIsImage} />}
         <textarea
+          ref={textarea}
+          rows={1}
           className={styles.contentInput}
           placeholder="함께 나누고 싶은 이야기를 적어주세요"
-          onChange={(e) => setContent(e.target.value)}
+          onChange={(e) => {
+            setContent(e.target.value)
+            handleResizeHeight()
+          }}
         />
       </div>
       <Footer setIsImage={setIsImage} setIsVote={setIsVote} />
