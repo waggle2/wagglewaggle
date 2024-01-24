134bb89516c2a2e7fb015649ec24f3e04e6eddff
hitzza <cslzh4433@gmail.com>
2024-01-24 11:03:32 +0900

    Feat: add swipeModule.css

--- app/_component/eventSlide/SwipeEvent.tsx
+++ app/_component/eventSlide/SwipeEvent.tsx
@@ -7,7 +7,34 @@ export default function SwipeEvent() {
   const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const divRef = useRef<HTMLDivElement>(null)
   const navRef = useRef<HTMLDivElement>(null)
-  console.log(divRef.current?.scrollWidth)
+  useEffect(() => {
+    const div = divRef.current
+    const nav = navRef.current
+    console.log('scrollwidth', divRef.current?.scrollWidth)
+    console.log('scrollreft', divRef.current?.scrollLeft)
+    console.log('offsetwidth', divRef.current?.offsetLeft)
+    console.log('children', divRef.current?.children)
+    if (!div || !nav) return
+
+    const updateSort = (element: HTMLDivElement) => {
+      const scrollWidth = element.scrollWidth
+      const scrollLeft = element.scrollLeft
+      const width = element.offsetWidth
+      const items = element.children
+
+      console.log(element, 'element')
+
+      if (scrollLeft <= width) {
+        element.prepend(items[items.length - 1])
+        element.scrollLeft = scrollLeft + width
+      }
+      if (scrollWidth - scrollLeft <= width) {
+        element.append(items[0])
+        element.scrollLeft = scrollLeft - width
+      }
+    }
+    updateSort(div)
+  }, [])
   return (
     <div className={style.containerWrapper}>
       <div className={style.container} ref={divRef}>
@@ -19,6 +46,7 @@ export default function SwipeEvent() {
           )
         })}
       </div>
+      <div className={style.currentPage}>1/10</div>
     </div>
   )
 }
