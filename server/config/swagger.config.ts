24cdcc3bca2678c8822f091cc52a8f00a65abf73
Jaesung Park <richdad6208@gmail.com>
2024-01-25 09:57:53 +0900

    Remove: Delete server folder

--- server/config/swagger.config.ts
+++
@@ -1,7 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger';
-
-export const swaggerConfig = new DocumentBuilder()
-    .setTitle('wagglewaggle API')
-    .setDescription('와글와글 API 문서입니다')
-    .setVersion('1.0')
-    .build();
