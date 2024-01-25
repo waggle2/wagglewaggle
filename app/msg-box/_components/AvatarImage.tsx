8d1f0636d2fa2ace230c5bd88a04db7ec9bd0883
Jaesung Park <richdad6208@gmail.com>
2024-01-25 13:20:55 +0900

    Remove: Delete avatar component

--- app/msg-box/_components/AvatarImage.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-
-export default function AvatarImage() {
-  return <Image src="/avatar.png" alt="avatar" width={40} height={40} />;
-}
--- app/msg-box/_components/MessagePreview.tsx
+++ app/msg-box/_components/MessagePreview.tsx
@@ -1,5 +1,4 @@
 import style from '../styles/messagePreview.module.scss'
-import AvatarImage from './AvatarImage'
 
 export default function MessagePreview({
   messageRoom,
@@ -8,9 +7,7 @@ export default function MessagePreview({
 }) {
   return (
     <article className={style.container}>
-      <span className={style.avatar}>
-        <AvatarImage />
-      </span>
+      <span className={style.avatar}></span>
       <div className={style.textDiv}>
         <span className={style.sender}>은하수</span>
         <span className={style.content}>여사친이 완전 못된 사람이네!!</span>
