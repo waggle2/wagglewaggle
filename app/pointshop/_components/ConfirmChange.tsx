7f8b676590db286db1da1c090dd971561fa8f51d
chang hee kim <changheekim@changui-MacBookAir.local>
2024-01-27 15:47:43 +0900

    Design: Modifying-pointshop-modal -ui

--- app/pointshop/_components/ConfirmChange.tsx
+++ app/pointshop/_components/ConfirmChange.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react'
 import style from '../_styles/confirmChange.module.scss'
 import Link from 'next/link'
 
@@ -8,6 +9,12 @@ type Props = {
     onConfirmClick: () => void,
 }
 export default function ConfirmChange({ pointDifference, selectedItemsLength, onConfirmClick }: Props) {
+    const [applyModal, setApplyModal] = useState(false);
+    const [applyProfile, setApplyProfile] = useState(false);
+
+    const applyModalHandler = () => {
+        setApplyModal(true);
+    }
 
     // 조건1: 상품이 담겨있고, 보유포인트 >= 상품가격 : 구매 최종 확인 모달
     // 조건2: 보유포인트-상품가격 < 0 일때 : 포인트 부족 안내 모달
@@ -19,12 +26,50 @@ export default function ConfirmChange({ pointDifference, selectedItemsLength, on
             return (
                 <div className={style.modalBackground}>
                     <div className={style.modal}>
-                        <p>현재 프로필을 <br />
-                            대표 프로필로 설정할까요?</p>
-                        <div className={style.buttonBox}>
-                            <button className={style.yesButton}>YES</button>
-                            <button onClick={onConfirmClick} className={style.noButton}>NO</button>
-                        </div>
+                        {!applyModal ? (
+                            <>
+                                <div className={style.textBox}>
+                                    <h4>아이템 구매</h4>
+                                    <span>선택하신 아이템을 구매하시겠어요?</span>
+                                </div>
+                                <div className={style.buttonBox}>
+                                    <button
+                                        className={style.noButton}
+                                        onClick={onConfirmClick}>
+                                        취소
+                                    </button>
+                                    <button
+                                        className={style.yesButton}
+                                        onClick={applyModalHandler}
+                                    >
+                                        구매하기
+                                    </button>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className={style.textBox}>
+                                    <h4>구매 완료</h4>
+                                    <span>
+                                        구매가 완료되었습니다. 잔여 고냥이포인트 : {pointDifference}
+                                    </span>
+                                </div>
+                                <div className={style.buttonBox}>
+                                    <button
+                                        className={style.noButton}
+                                        onClick={onConfirmClick}>
+                                        프로필 유지하기
+                                    </button>
+                                    <button
+                                        className={style.yesButton}
+                                        onClick={applyModalHandler}
+                                    >
+                                        프로필 설정하기
+                                    </button>
+                                </div>
+                            </>
+                        )}
+
                     </div>
                 </div>
             );
@@ -33,7 +78,10 @@ export default function ConfirmChange({ pointDifference, selectedItemsLength, on
             return (
                 <div className={style.modalBackground}>
                     <div className={style.modal}>
-                        <p>포인트가 부족합니다.</p>
+                        <div className={style.textBox}>
+                            <h4>구매 실패</h4>
+                            <span>잔여 포인트가 부족해 구입할 수 없어요.</span>
+                        </div>
                         <div className={style.buttonBox}>
                             <button onClick={onConfirmClick} className={style.closeButton}>닫기</button>
                         </div>
@@ -46,7 +94,10 @@ export default function ConfirmChange({ pointDifference, selectedItemsLength, on
         return (
             <div className={style.modalBackground}>
                 <div className={style.modal}>
-                    <p>상품을 선택해주세요.</p>
+                    <div className={style.textBox}>
+                        <h4>구매 실패</h4>
+                        <span>선택하신 아이템이 없어요.</span>
+                    </div>
                     <div className={style.buttonBox}>
                         <button onClick={onConfirmClick} className={style.closeButton}>닫기</button>
                     </div>
@@ -54,5 +105,7 @@ export default function ConfirmChange({ pointDifference, selectedItemsLength, on
             </div>
         );
     }
+
+
 }
 
