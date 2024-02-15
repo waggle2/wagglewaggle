2a99794f3e642f213394758fe973f335c0b0fea1
chang hee kim <changheekim@changui-MacBookAir.local>
2024-02-15 23:14:55 +0900

    Feat: apply-search-record-toggling

--- app/explore/_components/Explore.tsx
+++ app/explore/_components/Explore.tsx
@@ -20,15 +20,16 @@ const DeleteButton: React.FC<DeleteButtonProps> = ({ index, onDelete }) => {
 export default function Explore() {
     const [searchTerm, setSearchTerm] = useState<string>('');
     const [searchResults, setSearchResults] = useState<string[]>(['검색기록1', '검색기록2', '검색기록3', '검색기록4', '검색기록2', '검색기록3', '검색기록4', '검색기록2', '검색기록3', '검색기록4', '검색기록3', '검색기록4', '검색기록3', '검색기록4', '검색기록3', '검색기록4', '검색기록3', '검색기록4']);
-    // const tag = ['# 공감해줘', '# 격려해줘', '# 위로해줘', '# 조언해줘']
-
+    const [showSearchRecords, setShowSearchRecords] = useState<boolean>(true); // New state for managing visibility
 
     const handleDeleteSearchRecord = (index: number) => {
         const newSearchResults = searchResults.filter((_, i) => i !== index);
         setSearchResults(newSearchResults);
     };
 
-
+    const handleRecordSwitchChange = (checked: boolean) => {
+        setShowSearchRecords(checked);
+    };
 
     return (
         <div className={style.container}>
@@ -36,19 +37,21 @@ export default function Explore() {
 
             <div className={style.recordHeader}>
                 <div className={style.recentRecord}>최근 검색 기록</div>
-                <div className={style.recordSwitch}>검색 기록 숨기기<RecordSwitch /></div>
-
+                <div className={style.recordSwitch}>검색 기록 {showSearchRecords ? "숨기기" : "보기"}<RecordSwitch onChange={handleRecordSwitchChange} checked={showSearchRecords} /></div>
             </div>
 
-            <ul className={style.searchRecords}>
-                {searchResults.map((record, index) => (
-                    <li key={index} className={style.record}>
-                        <span>{record}</span>
-                        <DeleteButton index={index} onDelete={handleDeleteSearchRecord} />
-                    </li>
-                ))}
-            </ul>
+            {showSearchRecords && (
+                <ul className={style.searchRecords}>
+                    {searchResults.map((record, index) => (
+                        <li key={index} className={style.record}>
+                            <span>{record}</span>
+                            <DeleteButton index={index} onDelete={handleDeleteSearchRecord} />
+                        </li>
+                    ))}
+                </ul>
+            )}
 
         </div>
-    )
+    );
 }
+
