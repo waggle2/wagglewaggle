2aca889c0a4a6d17e7d207937f4160949e9d23dc
chang hee kim <changheekim@changui-MacBookAir.local>
2024-02-19 12:46:32 +0900

    Design: modify explore page style

--- app/explore/_components/Explore.tsx
+++ app/explore/_components/Explore.tsx
@@ -19,7 +19,7 @@ const DeleteButton: React.FC<DeleteButtonProps> = ({ index, onDelete }) => {
 
 export default function Explore() {
     const [searchTerm, setSearchTerm] = useState<string>('');
-    const [searchResults, setSearchResults] = useState<string[]>(['검색기록1', '검색기록2', '검색기록3', '검색기록4', '검색기록2', '검색기록3', '검색기록4', '검색기록2', '검색기록3', '검색기록4', '검색기록3', '검색기록4', '검색기록3', '검색기록4', '검색기록3', '검색기록4', '검색기록3', '검색기록4']);
+    const [searchResults, setSearchResults] = useState<string[]>(['검색기록1', '검색기록2', '검색기록3', '검색기록4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', 'LAST']);
     const [showSearchRecords, setShowSearchRecords] = useState<boolean>(true); // New state for managing visibility
 
     const handleDeleteSearchRecord = (index: number) => {
@@ -31,26 +31,37 @@ export default function Explore() {
         setShowSearchRecords(checked);
     };
 
+    const handleClearSearchRecord = () => {
+        setSearchResults([]);
+    }
+
     return (
         <div className={style.container}>
-            <SearchBar />
+            <div className={style.searchFnc}>
+                <SearchBar />
+                <div className={style.recordHeader}>
+                    <h4 >최근 검색 기록</h4>
+                    <button onClick={handleClearSearchRecord}>지우기</button>
+                    {/* <div className={style.recordSwitch}><span>검색 기록</span> {showSearchRecords ? "숨기기" : "보기"}<RecordSwitch onChange={handleRecordSwitchChange} checked={showSearchRecords} /></div> */}
+                </div>
 
-            <div className={style.recordHeader}>
-                <div className={style.recentRecord}>최근 검색 기록</div>
-                <div className={style.recordSwitch}>검색 기록 {showSearchRecords ? "숨기기" : "보기"}<RecordSwitch onChange={handleRecordSwitchChange} checked={showSearchRecords} /></div>
+
+
             </div>
 
             {showSearchRecords && (
-                <ul className={style.searchRecords}>
-                    {searchResults.map((record, index) => (
-                        <li key={index} className={style.record}>
-                            <span>{record}</span>
-                            <DeleteButton index={index} onDelete={handleDeleteSearchRecord} />
-                        </li>
-                    ))}
-                </ul>
-            )}
+                <div className={style.searchRecordContainer}>
 
+                    <ul className={style.searchRecords}>
+                        {searchResults.map((record, index) => (
+                            <li key={index} className={style.record}>
+                                <span>{record}</span>
+                                <DeleteButton index={index} onDelete={handleDeleteSearchRecord} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
