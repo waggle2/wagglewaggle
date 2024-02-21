6e10df9d2979aca0fbd851b3170f3d6185ee1ef6
Jaesung Park <richdad6208@gmail.com>
2024-02-21 15:02:40 +0900

    Chore: Delete msgbox layout style

--- app/msg-box/page.tsx
+++ app/msg-box/page.tsx
@@ -1,13 +1,18 @@
 import style from './styles/page.module.scss'
 import MessageRooms from './_components/MessageRooms'
 import Header from '../_components/common/header/page'
+import Footer from '../_components/common/footer/page'
+import PaddingProvider from '../_components/layoutSupport/PaddingProvider'
 export default function page() {
   return (
-    <div>
-      <Header isNoneSidePadding={true} leftSection={<h2>쪽지</h2>} />
-      <div className={style.roomsDiv}>
-        <MessageRooms />
-      </div>
+    <div className={style.container}>
+      <PaddingProvider>
+        <Header isNoneSidePadding={true} leftSection={<h2>쪽지</h2>} />
+        <div className={style.roomsDiv}>
+          <MessageRooms />
+        </div>
+      </PaddingProvider>
+      <Footer />
     </div>
   )
 }
