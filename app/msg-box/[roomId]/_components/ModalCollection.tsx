c12a2ebadf186cb2927023baf03015e66bf78ed2
Jaesung Park <richdad6208@gmail.com>
2024-02-29 22:06:51 +0900

    Refactor: Delete date formatter function and Use common formatter function

--- app/msg-box/[roomId]/_components/ModalCollection.tsx
+++ app/msg-box/[roomId]/_components/ModalCollection.tsx
@@ -19,19 +19,6 @@ export default function ModalCollection({
   const [blockStep, setBlockStep] = useState(0)
   const [deleteStep, setDeleteStep] = useState(0)
 
-  // const getCurrentDateTime = () => {
-  //   const now = new Date()
-
-  //   const year = now.getFullYear()
-  //   const month = String(now.getMonth() + 1).padStart(2, '0') // 월은 0부터 시작하므로 1을 더해줍니다.
-  //   const day = String(now.getDate()).padStart(2, '0')
-
-  //   const hours = String(now.getHours()).padStart(2, '0')
-  //   const minutes = String(now.getMinutes()).padStart(2, '0')
-
-  //   return `${year}-${month}-${day} ${hours}:${minutes}`
-  // }
-
   const handleDelete = () => {
     // 쪽지 삭제 로직
   }
