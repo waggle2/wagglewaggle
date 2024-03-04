56dbc95e16a98895e376dd52aec01506caa4902f
hitzza <cslzh4433@gmail.com>
2024-03-04 22:31:22 +0900

    Chore: modify next config rules option

--- next.config.mjs
+++ next.config.mjs
@@ -20,10 +20,22 @@ const nextConfig = {
     missingSuspenseWithCSRBailout: false,
   },
   webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    })
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
 
     config.plugins.push(new CompressionPlugin())
     if (!isServer) {
