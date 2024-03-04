0c0fe741acc8d07896feebc5b078e87a434c61e4
hitzza <cslzh4433@gmail.com>
2024-03-04 18:49:17 +0900

    Chore: add webpack optimization option

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,13 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import withPlugins from 'next-compose-plugins'
 import withBundleAnalyzer from '@next/bundle-analyzer'
+<<<<<<< HEAD
 import TerserPlugin from 'terser-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+=======
+import CompressionPlugin from 'compression-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+>>>>>>> e98b07a (Chore: add webpack optimization option)
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -20,6 +25,7 @@ const nextConfig = {
     missingSuspenseWithCSRBailout: false,
   },
   webpack: (config, { isServer }) => {
+<<<<<<< HEAD
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg'),
     )
@@ -56,6 +62,28 @@ const nextConfig = {
         }),
       ],
     }
+=======
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
+
+    config.plugins.push(new CompressionPlugin())
+    if (!isServer) {
+      config.mode = 'production' // 웹팩 모드 설정 추가
+    }
+    // TerserPlugin 추가
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            // TerserPlugin 옵션 설정 가능
+          },
+        }),
+      ],
+    }
+>>>>>>> e98b07a (Chore: add webpack optimization option)
 
     return config
   },
@@ -63,12 +91,16 @@ const nextConfig = {
 
 export default withPlugins(
   [
+<<<<<<< HEAD
     [
       withBundleAnalyzer,
       {
         enabled: process.env.ANALYZE === 'true', // ANALYZE=true일 때만 활성화
       },
     ],
+=======
+    [withBundleAnalyzer, { enabled: true, openAnalyzer: true }],
+>>>>>>> e98b07a (Chore: add webpack optimization option)
 
     // 다른 플러그인을 여기에 추가할 수 있습니다.
   ],
