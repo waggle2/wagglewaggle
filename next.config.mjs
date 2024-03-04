e98b07ad0a91ab59b6dfe6973e4dded2004a5ce0
hitzza <cslzh4433@gmail.com>
2024-03-04 18:49:17 +0900

    Chore: add webpack optimization option

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import { dirname } from 'path'
 import withPlugins from 'next-compose-plugins'
 import withBundleAnalyzer from '@next/bundle-analyzer'
 import CompressionPlugin from 'compression-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -23,17 +24,31 @@ const nextConfig = {
       test: /\.svg$/i,
       use: ['@svgr/webpack'],
     })
+
     config.plugins.push(new CompressionPlugin())
     if (!isServer) {
       config.mode = 'production' // 웹팩 모드 설정 추가
     }
+    // TerserPlugin 추가
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            // TerserPlugin 옵션 설정 가능
+          },
+        }),
+      ],
+    }
+
     return config
   },
 }
-// export default nextConfig
+
 export default withPlugins(
   [
     [withBundleAnalyzer, { enabled: true, openAnalyzer: true }],
+
     // 다른 플러그인을 여기에 추가할 수 있습니다.
   ],
   nextConfig,
