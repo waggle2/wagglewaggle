116cf8c10a969051c4ce17bdf6c6f7bbbb48b0d0
hitzza <cslzh4433@gmail.com>
2024-03-04 22:31:22 +0900

    Chore: modify next config rules option

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,8 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import withPlugins from 'next-compose-plugins'
 import withBundleAnalyzer from '@next/bundle-analyzer'
-<<<<<<< HEAD
-import TerserPlugin from 'terser-webpack-plugin'
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-=======
 import CompressionPlugin from 'compression-webpack-plugin'
 import TerserPlugin from 'terser-webpack-plugin'
->>>>>>> 4421ed4 (Chore: add webpack optimization option)
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -25,7 +20,6 @@ const nextConfig = {
     missingSuspenseWithCSRBailout: false,
   },
   webpack: (config, { isServer }) => {
-<<<<<<< HEAD
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg'),
     )
@@ -62,13 +56,7 @@ const nextConfig = {
         }),
       ],
     }
-=======
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    })
 
-    config.plugins.push(new CompressionPlugin())
     if (!isServer) {
       config.mode = 'production' // 웹팩 모드 설정 추가
     }
@@ -83,7 +71,6 @@ const nextConfig = {
         }),
       ],
     }
->>>>>>> 4421ed4 (Chore: add webpack optimization option)
 
     return config
   },
@@ -91,16 +78,7 @@ const nextConfig = {
 
 export default withPlugins(
   [
-<<<<<<< HEAD
-    [
-      withBundleAnalyzer,
-      {
-        enabled: process.env.ANALYZE === 'true', // ANALYZE=true일 때만 활성화
-      },
-    ],
-=======
     [withBundleAnalyzer, { enabled: true, openAnalyzer: true }],
->>>>>>> 4421ed4 (Chore: add webpack optimization option)
 
     // 다른 플러그인을 여기에 추가할 수 있습니다.
   ],
