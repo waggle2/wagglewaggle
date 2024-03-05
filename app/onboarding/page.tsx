f597d71856fd364267cbcb62927f753158782bbb
BoyoungYun <index_1234@naver.com>
2024-03-05 16:26:15 +0900

    Design: Apply onboarding page ui

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import Content from './_components/Content'
+import styles from './styles/page.module.scss'
+export default function Onboarding() {
+  return (
+    <div className={styles.container}>
+      <Content />
+    </div>
+  )
+}
