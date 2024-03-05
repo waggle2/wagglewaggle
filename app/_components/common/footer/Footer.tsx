8a5a060ad8cdbf4389955d7c72ce65a0a98472e1
Jaesung Park <richdad6208@gmail.com>
2024-03-05 14:26:41 +0900

    Design: Apply message alarm ui in footer navigation

--- app/_components/common/footer/Footer.tsx
+++ app/_components/common/footer/Footer.tsx
@@ -3,16 +3,22 @@
 import { usePathname } from '@/node_modules/next/navigation'
 import { useEffect, useState } from 'react'
 import style from './footer.module.scss'
-
 import GreenPuls from './_components/GreenPuls'
 import Home from './_components/Home'
 import Login from './_components/Login'
 import Massage from './_components/Massage'
 import MyPage from './_components/MyPage'
 import PointShop from './_components/PointShop'
+import { useGetAllMessageRooms } from '@/app/_hooks/services/queries/message'
 
 export default function Footer() {
   const pathname = usePathname()
+  const { data } = useGetAllMessageRooms()
+
+  const isUnreadMessage = !!data?.filter(
+    (room: any) => room.unreadMessageCount > 0,
+  ).length
+
   const [isLogin, setIsLogin] = useState<boolean>()
   useEffect(() => {
     setIsLogin(() => !!localStorage.getItem('isLogin'))
@@ -21,7 +27,10 @@ export default function Footer() {
   return (
     <footer className={style.container}>
       <Home path={pathname} />
-      <Massage path={pathname} />
+      <div className={style.messageDiv}>
+        <Massage path={pathname} />
+        {isUnreadMessage && <span className={style.alarm}></span>}
+      </div>
       <GreenPuls />
       <PointShop path={pathname} />
       {isLogin ? <MyPage path={pathname} /> : <Login />}
--- app/_hooks/services/queries/message.tsx
+++ app/_hooks/services/queries/message.tsx
@@ -11,6 +11,7 @@ export const useGetAllMessageRooms = () => {
   return useQuery({
     queryKey: ['message-rooms'],
     queryFn: getAllMessageBoxes,
+    staleTime: Infinity,
   })
 }
 
