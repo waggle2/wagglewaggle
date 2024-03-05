16bc43c15f8ae4f0b132472a6f3f6b241b4b0002
hitzza <cslzh4433@gmail.com>
2024-03-05 22:55:00 +0900

    Chore: add ANALYZE

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import withPlugins from 'next-compose-plugins'
 import withBundleAnalyzer from '@next/bundle-analyzer'
-import CompressionPlugin from 'compression-webpack-plugin'
 import TerserPlugin from 'terser-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -37,10 +37,14 @@ const nextConfig = {
       },
     )
 
-    config.plugins.push(new CompressionPlugin())
-    if (!isServer) {
-      config.mode = 'production' // 웹팩 모드 설정 추가
-    }
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        generateStatsFile: true,
+      }),
+    )
+    // if (!isServer) {
+    //   config.mode = 'production' // 웹팩 모드 설정 추가
+    // }
     // TerserPlugin 추가
     config.optimization = {
       minimize: true,
@@ -59,7 +63,12 @@ const nextConfig = {
 
 export default withPlugins(
   [
-    [withBundleAnalyzer, { enabled: true, openAnalyzer: true }],
+    [
+      withBundleAnalyzer,
+      {
+        enabled: process.env.ANALYZE === 'true', // ANALYZE=true일 때만 활성화
+      },
+    ],
 
     // 다른 플러그인을 여기에 추가할 수 있습니다.
   ],
