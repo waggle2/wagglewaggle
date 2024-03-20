3f3cd2e68e2b27b42ef8c6daa431de6f4f88def3
hitzza <cslzh4433@gmail.com>
2024-03-20 23:38:56 +0900

    Fix: Fixed an issue where profiles were broken when the screen was reduced

--- app/mypage/profileSetting/_components/CustomPreview.tsx
+++ app/mypage/profileSetting/_components/CustomPreview.tsx
@@ -25,69 +25,65 @@ export default function CustomPreview({
   return (
     <>
       {/* 선택된 동물 꾸미기 미리보기 */}
-      <div className={style.customContainer}>
-        <div className={style.customBackground}>
-          <div className={style.profilePreview}>
-            {isSetting && (
-              <Link
-                href={{
-                  pathname: '/mypage/profileSetting',
-                  query: { defaultAnimal: `${animal}` },
-                }}
-                className={style.setting}
-              >
-                <img
-                  src="/assets/setting.svg"
-                  alt="navigation profile setting"
-                />
-              </Link>
-            )}
-            <div className={style.imageContainer}>
-              <div className={style.imageBox}>
-                <div className={style.profileWrapper}>
-                  <img
-                    className={style.emoji}
-                    src={
-                      selectedEmoji
-                        ? selectedEmoji.image
-                        : `/assets/point_shop/emoji/${animal}_default.svg`
-                    }
-                    alt={'selectedEmoji'}
-                  />
 
-                  <img
-                    className={style.profileBg}
-                    src={
-                      selectedProfileBg
-                        ? selectedProfileBg.image
-                        : `/assets/point_shop/profile_background/${animal}_background_default.svg`
-                    }
-                    alt={'selectedProfileBg'}
-                  />
-                </div>
+      <article className={style.customBackground}>
+        <div className={style.profilePreview}>
+          {isSetting && (
+            <Link
+              href={{
+                pathname: '/mypage/profileSetting',
+                query: { defaultAnimal: `${animal}` },
+              }}
+              className={style.setting}
+            >
+              <img src="/assets/setting.svg" alt="navigation profile setting" />
+            </Link>
+          )}
+          <div className={style.imageContainer}>
+            <div className={style.imageBox}>
+              <div className={style.profileWrapper}>
                 <img
-                  className={style.frame}
+                  className={style.emoji}
                   src={
-                    selectedFrame
-                      ? selectedFrame.image
-                      : '/assets/point_shop/frame/frame_default.svg'
+                    selectedEmoji
+                      ? selectedEmoji.image
+                      : `/assets/point_shop/emoji/${animal}_default.svg`
                   }
-                  alt={'selectedFrame'}
+                  alt={'selectedEmoji'}
+                />
+
+                <img
+                  className={style.profileBg}
+                  src={
+                    selectedProfileBg
+                      ? selectedProfileBg.image
+                      : `/assets/point_shop/profile_background/${animal}_background_default.svg`
+                  }
+                  alt={'selectedProfileBg'}
                 />
               </div>
+              <img
+                className={style.frame}
+                src={
+                  selectedFrame
+                    ? selectedFrame.image
+                    : '/assets/point_shop/frame/frame_default.svg'
+                }
+                alt={'selectedFrame'}
+              />
             </div>
-            <img
-              className={style.wallPaper}
-              src={
-                selectedWallpaper
-                  ? selectedWallpaper.image
-                  : '/assets/point_shop/wallpaper/wallpaper_default.svg'
-              }
-              alt={'selectedWallpaper'}
-            />
           </div>
+          <img
+            className={style.wallPaper}
+            src={
+              selectedWallpaper
+                ? selectedWallpaper.image
+                : '/assets/point_shop/wallpaper/wallpaper_default.svg'
+            }
+            alt={'selectedWallpaper'}
+          />
         </div>
-      </div>
+      </article>
     </>
   )
 }
